4809dabdc558050139877037caec5c1c3b4725dc
jose ramon <rabohopo@gmail.com>
2021-09-08 09:23:59 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,31 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+
+    this.app = axios.create({baseURL: this.BASE_URL })
   }
 
   getFullList () {
+     return this.app.get('/characters')
+      
+  }
+
+  getOneRegister (id) {
+     return this.app.get(`/characters/${id}`)
+  }
+
+  createOneRegister (characterInfo ) {
+     return this.app.post('/characters',characterInfo)
+  }
+
+  updateOneRegister (id) {
+
+     // return this.app.put()
 
   }
 
-  getOneRegister () {
+  deleteOneRegister (id) {
 
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+      return this.app.delete(`/characters/${id}`)
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,131 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com');
+
+function changeError() {
+  console.log('error');
+  const color = document.querySelector('#send-data')
+  color.style.backgroundColor = 'red'
+  return false
+}
+
+function changeSucces() {
+
+  const color = document.querySelector('#send-data')
+  color.style.backgroundColor = 'green'
+}
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+    event.preventDefault()
+    charactersAPI
+      .getFullList()
+      .then(response => {
+
+        let text = ' '
+        response.data.forEach(elm => {
+
+          text += ` <div class="character-info">
+                <div class="name">${elm.name}(${elm.id})</div>
+                <div class="occupation">${elm.occupation}</div>
+                <div class="cartoon">${elm.cartoon}</div>
+                <div class="weapon">${elm.weapon}</div>
+              </div>`
+          document.querySelector('.character-info').innerHTML = text
+        })
+
+      })
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    event.preventDefault()
+    const valueId = document.querySelector('.operation input').value
+
+    charactersAPI
+      .getOneRegister(valueId)
+      .then(response => {
+        let text = ''
+
+        console.log(response.data)
+
+
+        text += ` <div class="character-info">
+                 <div class="name">${response.data.name}(${response.data.id})</div>
+                 <div class="occupation">${response.data.occupation}</div>
+                 <div class="cartoon">${response.data.cartoon}</div>
+                 <div class="weapon">${response.data.weapon}</div>
+                 </div>`
+
+        document.querySelector('.characters-container').innerHTML = text
+      })
+
+
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+
+    event.preventDefault()
+
+    const valueId = document.querySelector('.operation.delete input').value
+
+
+    charactersAPI
+      .deleteOneRegister(valueId)
+      .then((minion) => {
+        console.log(minion)
+        if (minion.data) {
+          const color = document.querySelector('#delete-one')
+          color.style.backgroundColor = 'green'
+          return
+        } else {
+          const color = document.querySelector('#delete-one')
+          color.style.backgroundColor = 'red'
+        }
+
+      })
+      .catch((err) => {
+        console.log('its not workig', err)
+      })
+
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
+
+
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    const inputs = document.querySelectorAll('#new-character-form input')
+
+    const character = {
+      name: inputs[0].value,
+      occupation: inputs[1].value,
+      weapon: inputs[2].value,
+      cartoon: inputs[3].checked
+    }
+
+    Object.values(character).every(function(elm){
+      console.log(elm.length)
+      if(elm.length == 0){
+        return changeError()
+      } else {
+       create()
+      }
+    })
+
+function create(){
+      console.log('string creo');
+      charactersAPI
+        .createOneRegister(character)
+        .then((minions) => {
+          changeSucces()
+        })
+      }
+
 
   });
 });
